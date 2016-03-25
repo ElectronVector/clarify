d5a4e61fb65be903992ecabc5596d42ba91ccddb
Matt Chernosky <mchernosky@syncroness.com>
2016-03-25 14:53:14 -0600

    Updated readme.

--- rakefile.rb
+++ rakefile.rb
@@ -3,7 +3,7 @@ task :default => %w[ behav ]
 
 task :behav do
     # Generate the preprocessed file.
-    sh "gcc -E test_behav.c > test_behav.preprocessed"
+    # sh "gcc -E test_behav.c > test_behav.preprocessed"
     # Build and run the test binary.
     sh "gcc -W -Wall test_behav.c -o behav.exe"
     sh "./behav.exe" do |ok|
